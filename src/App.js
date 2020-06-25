c03461928fcd2260f27ea64b2cfd8a88055e3597
dmjusto <31903571+dmjusto@users.noreply.github.com>
2020-06-24 23:47:00 -0600

    add palette.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import Palette from './palette';
+import seedColors from './seedColors';
 
 function App() {
   return (
     <div className="App">
-      
+      <Palette {...seedColors[4]}/>
     </div>
   );
 }
---
+++ src/palette.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                {/* navBar goes here */}
+                <div className="palette-colors">
+                    {/* bunch of color boxes */}
+                </div>
+                {/* footer goes here */}
+            </div>
+        )
+    }
+}

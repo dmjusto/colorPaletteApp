9effc989175f2ecac324634bf17399998d906d04
dmjusto <31903571+dmjusto@users.noreply.github.com>
2020-07-04 19:56:26 -0600

    add savePalette

--- src/App.js
+++ src/App.js
@@ -9,17 +9,30 @@ import {generatePalettes} from './colorHelpers';
 // import palette from './palette';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      palettes: seedColors
+    }
+    this.savePalette = this.savePalette.bind(this);
+    this.findPalette = this.findPalette.bind(this);
+  }
+
   findPalette(id){
-    return seedColors.find(palette => {
+    return this.state.palettes.find(palette => {
       return palette.id === id
     });
+  }
+
+  savePalette(newPalette){
+    this.setState({palettes: [...this.state.palettes, newPalette]})
   }
 
   render(){
     return (
       <Switch>
-        <Route exact path='/palette/new' render={() => <NewPaletteForm/>}/>
-        <Route exact path='/' render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps}/>}/>
+        <Route exact path='/palette/new' render={(routeProps) => <NewPaletteForm savePalette={this.savePalette} {...routeProps}/>}/>
+        <Route exact path='/' render={(routeProps) => <PaletteList palettes={this.state.palettes} {...routeProps}/>}/>
         <Route exact path='/palette/:id' render={(routProps) => <Palette palette={generatePalettes(this.findPalette(routProps.match.params.id))}/>}/>
         <Route exact path='/palette/:paletteId/:colorId' render={(routeProps) => 
           <SingleColorPalette

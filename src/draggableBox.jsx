9b6272ddb45483e37c5c58529e58ba4fdfdc45c8
dmjusto <31903571+dmjusto@users.noreply.github.com>
2020-07-05 18:28:24 -0600

    draggable box style

--- src/draggableBox.jsx
+++ src/draggableBox.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {withStyles} from '@material-ui/styles';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 const styles = {
     root: {
@@ -10,13 +11,38 @@ const styles = {
         position: 'relative',
         cursor: 'pointer',
         marginBottom: '-4px',
+        '&:hover svg' : {
+            color: 'white',
+            transform: 'scale(1.5)'
+        }
+    },
+    boxContent: {
+        position: 'absolute',
+        padding: '10px',
+        width: '100%',
+        left: '0',
+        bottom: '0',
+        color: 'rbga(0, 0, 0, 0.5)',
+        letterSpacing: '1px',
+        textTransform: 'uppercase',
+        fontSize: '12px',
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    deleteIcon: {
+        color: 'rgba(0, 0, 0, 0.5)',
+        transition: 'all 0.3s ease-in-out'
     }
 }
 
 function draggableBox(props) {
+    const {classes} = props;
     return (
-        <div className={props.classes.root} style={{backgroundColor: props.color}}>
-            {props.name}
+        <div className={classes.root} style={{backgroundColor: props.color}}>
+            <div className={classes.boxContent}>
+                <span>{props.name}</span>
+                <DeleteForeverIcon className={classes.deleteIcon}/>
+            </div>
         </div>
     )
 }
